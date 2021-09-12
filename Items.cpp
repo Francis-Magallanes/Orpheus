f111f20d59ecac1e871a19caf53b5d7bfc4712ac
Francis John Magallanes <60374264+Francis-Magallanes@users.noreply.github.com>
2021-09-12 14:13:33 +0800

    adjustments

--- Items.cpp
+++ Items.cpp
@@ -8,24 +8,31 @@ Items::Items(TypeItems type, uint32_t* sprites) {
 
 		case TypeItems::COLLECTABLE1:
 			sprite = sprites;
+			hitpoints = 1;
 			break;
 		case TypeItems::COLLECTABLE2:
 			sprite = sprites + (BLOCK_WIDTH);
+			hitpoints = 1;
 			break;
 		case TypeItems::COLLECTABLE3:
 			sprite = sprites + (BLOCK_WIDTH * 2);
+			hitpoints = 1;
 			break;
 		case TypeItems::WEAPON:
 			sprite = sprites + (BLOCK_WIDTH * 18);
+			hitpoints = 100; // hitspoints for weapon will be used as basis on the number of attacks
 			break;
 		case TypeItems::HEAL:
 			sprite = sprites + (BLOCK_WIDTH * 19);
+			hitpoints = 20; //hitspoints for heal will be used as basis on the number of heal will happen
 			break;
 		case TypeItems::CREEP:
-			sprite = sprites + (BLOCK_WIDTH * 19);
+			sprite = sprites + (BLOCK_WIDTH * 20);
+			hitpoints = 20; //this will also the basis for the damage that player will receive
 			break;
 		case TypeItems::CERBERUS:
 			sprite = sprites + (BLOCK_WIDTH * 22);
+			hitpoints = 100000;
 			break;
 
 		default: //it will act as collectable 1
@@ -34,10 +41,13 @@ Items::Items(TypeItems type, uint32_t* sprites) {
 	}
 }
 
+
 void Items::absorbDamage(int damage) {
 	hitpoints = hitpoints - damage;
 }
 
-int Items::getHitpoints() { return hitpoints; }
-
 TypeItems Items::getType() { return type; }
+
+bool Items::isItemsInstance(){
+	return true;
+}
